50b03fcf55d3136ce70972774fa7ffc2d58106c1
Sam <138508687+samuelm-LCWC@users.noreply.github.com>
2024-09-24 16:09:37 +0100

    Update test_task.py

--- test_task.py
+++ test_task.py
@@ -11,13 +11,30 @@ def test_task_1_values():
     for i in result:
         assert 1 <= i <= 49
 
-@pytest.mark.parametrize("endpoint", [90, 80, 70, 60])
-def test_task_2(endpoint):
-    with patch("builtins.input", return_value=(str(endpoint))):
+def test_task_2_90():
+    with patch("builtins.input", return_value="90"):
         result = task_2()
-        check_list = [i for i in range(100, endpoint - 1, -1)]
+        check_list = [i for i in range(100, 89, -1)]
         assert result == check_list
 
+def test_task_2_80():
+    with patch("builtins.input", return_value="80"):
+        result = task_2()
+        check_list = [i for i in range(100, 79, -1)]
+        assert result == check_list
+
+def test_task_2_70():
+    with patch("builtins.input", return_value="70"):
+        result = task_2()
+        check_list = [i for i in range(100, 69, -1)]
+        assert result == check_list
+
+def test_task_2_60():
+    with patch("builtins.input", return_value="60"):
+        result = task_2()
+        check_list = [i for i in range(100, 59, -1)]
+        assert result == check_list
+        
 def test_task_3():
     result = task_3
     assert len(result) == 3
